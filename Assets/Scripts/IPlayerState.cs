d5590871910870cd06fa3e3847e110277c731ace
chlghksgml01 <58538862+chlghksgml01@users.noreply.github.com>
2025-03-30 11:41:22 +0900

    에셋, 플레이어 다시 설정

---
+++ Assets/Scripts/IPlayerState.cs
@@ -0,0 +1,59 @@
+
+public interface IPlayerState
+{
+    public void EnterState(Movement movement);
+    public void UpdateState(Movement movement);
+    public void ExitState(Movement movement);
+}
+
+public class PlayerIdleState : IPlayerState
+{
+    public void EnterState(Movement movement)
+    {
+        movement.SetAnimBool("isMoving", false);
+    }
+
+    public void UpdateState(Movement movement)
+    {
+        if (movement.MoveInput.magnitude > 0)
+        {
+            movement.ChangeState(movement.moveState);
+
+            movement.SetAnimBool("isMoving", true);
+        }
+    }
+
+    public void ExitState(Movement movement)
+    {
+        movement.SetAnimBool("isMoving", true);
+    }
+}
+
+public class PlayerMoveState : IPlayerState
+{
+    public void EnterState(Movement movement)
+    {
+        movement.SetAnimBool("isMoving", true);
+    }
+
+    public void UpdateState(Movement movement)
+    {
+        if (movement.MoveInput.magnitude > 0)
+        {
+            movement.SetAnimFloat("horizontal", movement.MoveInput.x);
+            movement.SetAnimFloat("vertical", movement.MoveInput.y);
+        }
+
+        else if (movement.MoveInput.magnitude == 0)
+        {
+            movement.ChangeState(movement.idleState);
+        }
+    }
+
+    public void ExitState(Movement movement)
+    {
+        movement.SetAnimBool("isMoving", false);
+    }
+}
+
+
