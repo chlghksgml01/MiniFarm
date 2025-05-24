87f45451cbdd8659ff443df7c0aa6ff47be0ec29
chlghksgml01 <chlghksgml01@naver.com>
2025-05-24 10:59:13 +0900

    [chore] AnimationCurve 삭제하고 변수로 값 조절

--- Assets/4Scripts/DayTimeController.cs
+++ Assets/4Scripts/DayTimeController.cs
@@ -2,11 +2,14 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-
 public class DayTimeController : MonoBehaviour
 {
     [Header("Time")]
     [SerializeField] int dayStartTime = 6;
+    [SerializeField] int dayEndTime = 26;
+    [SerializeField] int twilightStartTime = 19;
+    [SerializeField] int darkestTime = 22;
+
     [SerializeField] float timeScale = 60f;
     [SerializeField] float timeInterval = 10f;
     [SerializeField] TextMeshProUGUI timeText;
@@ -15,7 +18,8 @@ public class DayTimeController : MonoBehaviour
     [SerializeField] Light2D globalLight;
     [SerializeField] Color nightLightColor;
     [SerializeField] Color dayLightColor = Color.white;
-    [SerializeField] AnimationCurve nightTimeCurve;
+
+    const int secondsPerHour = 3600;
 
     private float realTimer = 0f;
     private float gameTimer = 0f;
@@ -24,10 +28,10 @@ public class DayTimeController : MonoBehaviour
 
     private void Awake()
     {
-        gameTimer = dayStartTime * 3600;
+        gameTimer = dayStartTime * secondsPerHour;
         timeText.text = string.Format("{0:00}:{1:00}", dayStartTime, minute);
     }
-
+    
     private void Update()
     {
         UpdateTime();
@@ -41,11 +45,10 @@ public class DayTimeController : MonoBehaviour
             realTimer = 0f;
             gameTimer += timeInterval * timeScale;
 
-            hour = (int)(gameTimer / 3600) % 24;
+            hour = (int)(gameTimer / secondsPerHour) % 24;
             minute = (int)(gameTimer / 60) % 60;
 
-            // »õº® 2½Ã 
-            if (gameTimer >= 93600f)
+            if (gameTimer >= dayEndTime * secondsPerHour)
             {
                 NextDay();
             }
@@ -61,14 +64,15 @@ public class DayTimeController : MonoBehaviour
 
     private void NextDay()
     {
-        gameTimer = dayStartTime * 3600;
+        gameTimer = dayStartTime * secondsPerHour;
         hour = dayStartTime;
     }
 
     private void UpdateLight()
     {
-        float v = nightTimeCurve.Evaluate(gameTimer);
-        Color color = Color.Lerp(dayLightColor, nightLightColor, v);
+        float lightLerpTime = Mathf.InverseLerp(twilightStartTime * secondsPerHour, darkestTime * secondsPerHour, gameTimer);
+
+        Color color = Color.Lerp(dayLightColor, nightLightColor, lightLerpTime);
         globalLight.color = color;
     }
 }
